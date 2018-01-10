b7c68342d88d3b8bec186ed2262e7fee08bcbc7f
Dan Lebrero <dlebrero@gmail.com>
2018-01-10 16:15:19 +0100

    removing some duplication

--- src/gae_to_kafka_initial_import/io/gae.clj
+++ src/gae_to_kafka_initial_import/io/gae.clj
@@ -9,22 +9,23 @@
     :hostname (str (:org-id org-config) ".appspot.com")
     :port 443))
 
+(defn iter [next-thunk next-fn]
+  (lazy-seq
+    (let [result (next-thunk)]
+      (when (seq result)
+        (concat result
+                (iter (partial next-fn result) next-fn))))))
+
 (defn fetch-events
   [config process-fn]
   (gae/with-datastore [ds (datastore-spec config)]
     (let [query (.prepare ds (query/query {:kind (:kind config)}))
-          batch-size 300
-          more-results (fn more-results [query-result]
-                         (lazy-seq
-                           (let [cursor (.getCursor query-result)
-                                 next-query-result (.asQueryResultList query
-                                                                       (query/fetch-options {:limit        batch-size
-                                                                                             :start-cursor cursor}))]
-                             (when (seq next-query-result)
-                               (concat next-query-result
-                                       (more-results next-query-result))))))]
-      (process-fn (lazy-seq
-                    (let [query-result (.asQueryResultList query (query/fetch-options {:limit batch-size}))]
-                      (when (seq query-result)
-                        (concat query-result
-                                (more-results query-result)))))))))
+          batch-size 300]
+      (process-fn
+        (iter
+          #(.asQueryResultList query (query/fetch-options {:limit batch-size}))
+          (fn [query-result]
+            (let [cursor (.getCursor query-result)]
+              (.asQueryResultList query
+                                  (query/fetch-options {:limit        batch-size
+                                                        :start-cursor cursor})))))))))
