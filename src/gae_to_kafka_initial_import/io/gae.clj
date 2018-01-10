1db153bdf1d555c87768eec89d203a86a4895679
Dan Lebrero <dlebrero@gmail.com>
2018-01-10 14:42:43 +0100

    GAE store as lazy sequence

--- src/gae_to_kafka_initial_import/io/gae.clj
+++ src/gae_to_kafka_initial_import/io/gae.clj
@@ -2,7 +2,6 @@
   (:require
     [akvo.commons.gae :as gae]
     [akvo.commons.gae.query :as query]
-    [clojure.tools.logging :as log]
     [clojure.java.io :refer (writer)]))
 
 (defn datastore-spec [org-config]
@@ -11,25 +10,33 @@
     :port 443))
 
 (defn fetch-events
-  [config f]
-  (gae/with-datastore
-    [ds (datastore-spec config)]
+  [config process-fn]
+  (gae/with-datastore [ds (datastore-spec config)]
     (let [query (.prepare ds (query/query {:kind (:kind config)}))
-          first-query-result (.asQueryResultList query (query/fetch-options {:limit 300}))]
-      (loop [query-result first-query-result]
-        (when-not (empty? query-result)
-          (let [event-count (count query-result)
-                _ (log/debugf "Read %s events from %s" event-count (:org-id config))
-                process-result (f query-result)]
-            (case process-result
-              ::more (let [cursor (.getCursor query-result)
-                           next-query-result (.asQueryResultList query
-                                                                 (query/fetch-options {:limit        300
-                                                                                       :start-cursor cursor}))]
-                       (recur next-query-result))
-              process-result)))))))
+          batch-size 300
+          more-results (fn more-results [query-result]
+                         (lazy-seq
+                           (let [cursor (.getCursor query-result)
+                                 next-query-result (.asQueryResultList query
+                                                                       (query/fetch-options {:limit        batch-size
+                                                                                             :start-cursor cursor}))]
+                             (concat next-query-result
+                                     (more-results next-query-result)))))]
+      (process-fn (lazy-seq
+                    (let [query-result (.asQueryResultList query (query/fetch-options {:limit batch-size}))]
+                      (when (seq query-result)
+                        (concat query-result
+                                (more-results query-result)))))))))
+
+
 
 (comment
+
+  (fetch-events {:service-account-id "sa-akvoflowsandbox@akvoflowsandbox.iam.gserviceaccount.com"
+                 :org-id             "akvoflowsandbox"
+                 :kind               "SurveyInstance"
+                 :private-key-file   "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflowsandbox/akvoflowsandbox.p12"}
+                (fn [x] (gae-to-kafka-initial-import.io.local-file/write-as-serialized "foo.dat" (take 10 x))))
 
   (fetch-and-insert-new-events {:service-account-id "sa-akvoflowsandbox@akvoflowsandbox.iam.gserviceaccount.com"
                                 :org-id             "akvoflowsandbox"
