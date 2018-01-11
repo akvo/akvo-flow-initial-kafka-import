0291f923f08c2b3df4a8467aa9a74e4c772fd7d7
Dan Lebrero <dlebrero@gmail.com>
2018-01-11 10:02:07 +0100

    minor clean up

--- src/gae_to_kafka_initial_import/gae_entity.clj
+++ src/gae_to_kafka_initial_import/gae_entity.clj
@@ -1,6 +1,8 @@
 (ns gae-to-kafka-initial-import.gae-entity
+  "Adapted from https://github.com/migae/datastore/blob/master/src/clj/migae/datastore/types/entity_map.clj"
   (:require [clojure.edn :as edn])
-  (:import (com.google.appengine.api.datastore Key EmbeddedEntity Email Link Entity)))
+  (:import (com.google.appengine.api.datastore Key EmbeddedEntity Email Link Entity)
+           (java.util Date Collections$UnmodifiableMap Collection)))
 
 (declare ds-to-clj-coll ds-to-clj)
 
@@ -16,21 +18,20 @@
 
 (defn ds-to-clj
   [v]
-  ;; (log/trace "ds-to-clj:" v (type v) (class v))
   (let [val (cond (integer? v) v
                   (string? v) (str v)
-                  (= (class v) java.lang.Double) v
-                  (= (class v) java.lang.Boolean) v
-                  (= (class v) java.util.Date) v
+                  (= (class v) Double) v
+                  (= (class v) Boolean) v
+                  (= (class v) Date) v
 
-                  (= (class v) java.util.Collections$UnmodifiableMap)
+                  (= (class v) Collections$UnmodifiableMap)
                   (let [props v]
                     (into {} (for [[k v] props]
                                (let [prop (keyword k)
                                      val (ds-to-clj v)]
                                  {prop val}))))
 
-                  (instance? java.util.Collection v) (ds-to-clj-coll v)
+                  (instance? Collection v) (ds-to-clj-coll v)
                   (= (type v) Link) (.toString v)
 
                   (= (type v) Email) (.getEmail v)
@@ -43,20 +44,14 @@
                   (= (type v) Key) (let [kind (.getKind v)]
                                      (if (= kind "Keyword")
                                        (keyword (.getName v))
-                                       ;; (symbol (.getName v))))
                                        (str \' (.getName v))))
                   (nil? v) nil
-                  :else (do
-                          ;; (log/trace "HELP: ds-to-clj else " v (type v))
-                          (throw (RuntimeException. (str "HELP: ds-to-clj else [" v "][" (class v) "]" (nil? v))))
-                          (edn/read-string v)))]
-    ;; (println "ds-to-clj result:" v val)
+                  :else (throw (RuntimeException. (str "HELP: ds-to-clj else [" v "][" (class v) "]" (nil? v)))))]
     val))
 
-(defn ds-to-clj-coll
+(defn- ds-to-clj-coll
   "Type conversion: java datastore to clojure"
   [coll]
-  ;; (log/trace "ds-to-clj-coll" coll (type coll))
   (cond
     (= (type coll) java.util.ArrayList) (into '() (for [item coll]
                                                     (ds-to-clj item)))
@@ -64,5 +59,4 @@
                                                    (ds-to-clj item)))
     (= (type coll) java.util.Vector)  (into [] (for [item coll]
                                                  (ds-to-clj item)))
-    :else (throw (ex-info "???" {:c coll}))
-    ))
+    :else (throw (ex-info "???" {:c coll}))))
