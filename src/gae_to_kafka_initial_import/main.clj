644ae210c78fddb94276756adb216357cee0beca
Dan Lebrero <dlebrero@gmail.com>
2018-01-12 18:41:47 +0100

    renaming ns

--- src/gae_to_kafka_initial_import/main.clj
+++ src/gae_to_kafka_initial_import/main.clj
@@ -1,35 +1,76 @@
 (ns gae-to-kafka-initial-import.main
-  (:require [gae-to-kafka-initial-import.io.gae :as gae]
-            [gae-to-kafka-initial-import.io.local-file :as local-file]
-            [gae-to-kafka-initial-import.io.kafka-rest-proxy :as kafka]
-            [gae-to-kafka-initial-import.gae-entity :as gae-entity]
-            [gae-to-kafka-initial-import.util :as util]
-            [thdr.kfk.avro-bridge.core :as avro]
-            [gae-to-kafka-initial-import.avro-schema :as avro-schema]
+  (:require [clojure.java.io :as io]
+            [gae-to-kafka-initial-import.core :as initial-import]
+            [medley.core :as medley]
             [camel-snake-kebab.core :refer [->camelCase ->kebab-case]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log])
+  (:import (com.google.apphosting.utils.config AppEngineWebXmlReader)))
+
+(defn instance-info
+  [^java.io.File file]
+  (let [config (-> file
+                   .getAbsolutePath
+                   (AppEngineWebXmlReader. "")
+                   .readAppEngineWebXml)
+        org-id (.getAppId config)]
+    (medley/map-keys
+      (fn [k] (-> k name ->kebab-case keyword))
+      (into {:org-id           org-id
+             :private-key-file (.getAbsolutePath (io/file (-> file .getParentFile) (str org-id ".p12")))}
+            (.getSystemProperties config)))))
+
+(defn find-all-enabled-tentants [dir]
+  (->> dir
+       io/file
+       file-seq
+       (filter (fn [f] (re-find #"appengine-web.xml$" (.getName f))))
+       (map instance-info)
+       (map #(select-keys % [:org-id :service-account-id :windshaft-maps :private-key-file]))
+       (filter #(-> % :windshaft-maps (= "true")))
+       (sort-by :org-id)))
+
+(defn local-file-for [org-id kind]
+  (str org-id "." kind ".binary.txt"))
+
+(defn kafka-topic [org-id kind]
+  (->
+    (str "flow." org-id "." kind ".avro.v1")
+    clojure.string/lower-case
+    (clojure.string/replace #"[^a-z0-9\.]" "_")))
+
+(defn download-from-gae [kind instances]
+  (doseq [instance instances]
+    (initial-import/gae->local-file
+      (assoc instance :kind kind)
+      (local-file-for (:org-id instance) kind))))
+
+(defn validate-against-schema [schema kind instances]
+  (doseq [instance instances]
+    (let [report (initial-import/report-schema-compliance
+                   (local-file-for (:org-id instance) kind)
+                   schema)]
+      (log/info "Schema report for" (:org-id instance) "kind" kind)
+      (initial-import/log-report report)
+      (when (initial-import/errors report)
+        (throw (RuntimeException. (str "Schema errors. See logs")))))))
+
+(defn push-to-kafka [schema kind instances]
+  (doseq [instance instances]
+    (initial-import/local-file->kafka-avro
+      (local-file-for (:org-id instance) kind)
+      (kafka-topic (:org-id instance) kind)
+      schema)))
 
 (comment
 
-  (check-local-file-against-schema "gae-internal2.strs" (org.akvo.flow.avro.DataPoint/getClassSchema))
-  (check-local-file-against-schema "gae.strs" (org.akvo.flow.avro.DataPoint/getClassSchema))
+  (def kind "SurveyedLocale")
+  (def instances (find-all-enabled-tentants "/Users/dlebrero/projects/akvo/akvo-flow-server-config/"))
+  (def instances (find-all-enabled-tentants "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflowsandbox"))
+  (def schema (org.akvo.flow.avro.DataPoint/getClassSchema))
 
-  (gae->local-file {:service-account-id "sa-akvoflow-internal2@akvoflow-internal2.iam.gserviceaccount.com"
-                    :org-id             "akvoflow-internal2"
-                    :kind               "SurveyedLocale"
-                    :private-key-file   "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflow-internal2/akvoflow-internal2.p12"}
-                   "gae-internal2.strs")
-
-  (gae->local-file {:service-account-id "sa-akvoflowsandbox@akvoflowsandbox.iam.gserviceaccount.com"
-                    :org-id             "akvoflowsandbox"
-                    :kind               "SurveyedLocale"
-                    :private-key-file   "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflowsandbox/akvoflowsandbox.p12"}
-                   "gae.strs")
-
-  (local-file->kafka-binary "gae.strs" "binarytest")
-
-  (local-file->kafka-avro "gae.strs" "nonbinarytest" (org.akvo.flow.avro.DataPoint/getClassSchema))
-
+  (download-from-gae kind instances)
+  (validate-against-schema schema kind instances)
+  (push-to-kafka schema kind instances)
   (local-file/read-from "gae-internal2.strs"
                         (comp
                           (fn [stats] (gae-to-kafka-initial-import.avro-schema/->avro stats "Foo"))
@@ -38,21 +79,4 @@
                                          gae-entity->clj
                                          bytes->obj
                                          str->bytes))))
-
-
-  (fetch-and-insert-new-events {:service-account-id "sa-akvoflowsandbox@akvoflowsandbox.iam.gserviceaccount.com"
-                                :org-id             "akvoflowsandbox"
-                                :kind               "SurveyInstance"
-                                :private-key-file   "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflowsandbox/akvoflowsandbox.p12"})
-
-  (fetch-and-insert-new-events {:service-account-id "sa-akvoflowsandbox@akvoflowsandbox.iam.gserviceaccount.com"
-                                :org-id             "akvoflowsandbox"
-                                :kind               "QuestionOption"
-                                :private-key-file   "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflowsandbox/akvoflowsandbox.p12"})
-
-  (fetch-and-insert-new-events {:service-account-id "sa-akvoflowsandbox@akvoflowsandbox.iam.gserviceaccount.com"
-                                :org-id             "akvoflowsandbox"
-                                :kind               "SurveyedLocale"
-                                :private-key-file   "/Users/dlebrero/projects/akvo/akvo-flow-server-config/akvoflowsandbox/akvoflowsandbox.p12"})
-
   )
